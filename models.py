db2e0e83d05d0db53045f72d6dc7234f0ec29061
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2021-05-04 17:21:04 -0700

    created all the base stand alone functions

---
+++ models.py
@@ -0,0 +1,35 @@
+#Create the train/test split
+def get_split(X,y):
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=101)
+    return(X_train, X_test, y_train, y_test)
+
+#Standardize the data using StandardScaler
+def standardize(X_train,X_test):
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    return (X_train,X_test)
+
+#Baseline Random forest model
+def rfc(X_train,y_train,X_test):
+    from sklearn.ensemble import RandomForestClassifier
+    rfc = RandomForestClassifier(n_estimators=10, criterion = 'entropy', random_state = 0)
+    rfc.fit(X_train,y_train)
+    y_pred = rfc.predict(X_test)
+    return y_pred
+
+#Baseline Logistic regression model
+def def log_reg(X_train,y_train,X_test):
+    from sklearn.linear_model import LogisticRegression
+    log_cla = LogisticRegression(random_state = 0)
+    log_cla.fit(X_train, y_train)
+    y_pred = log_cla.predict(X_test)
+    return y_pred
+
+def get_results(y_test, y_pred):
+    from sklearn.metrics import confusion_matrix, accuracy_score
+    cm = confusion_matrix(y_test, log_pred)
+    accuracy_score(y_test, log_pred)
+    return (cm, accuracy_score)
