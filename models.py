5d7e1ff2b3a00fe43d878aa8479e57e6828991bf
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2021-05-14 18:09:31 -0700

    added a function to run linear regression

--- models.py
+++ models.py
@@ -28,7 +28,14 @@ def log_reg(X_train,y_train,X_test):
     y_pred = log_cla.predict(X_test)
     return y_pred
 
-def get_results(y_test, y_pred):
+def lin_reg(X_train,y_train,X_test):
+    from sklearn.linear_model import LinearRegression
+    regressor = LinearRegression()
+    regressor.fit(X_train,y_train)
+    y_pred = regressor.predict(X_test)
+    return y_pred
+
+def get_class_results(y_test, y_pred):
     from sklearn.metrics import confusion_matrix, accuracy_score
     cm = confusion_matrix(y_test, y_pred)
     score = round(accuracy_score(y_test, y_pred),5)
@@ -39,7 +46,7 @@ def run_rfc(X,y):
     X_train, X_test, y_train, y_test = get_split(X,y)
     y_pred = rfc(X_train,y_train,X_test)
     get_results(y_test, y_pred)
-    cm, score = get_results(y_test, y_pred)
+    cm, score = get_class_results(y_test, y_pred)
     print(cm)
     print("")
     print(f"The accuracy of the model is {score}")
@@ -48,7 +55,12 @@ def run_log_reg(X,y):
     X_train, X_test, y_train, y_test = get_split(X,y)
     X_train, X_test = standardize(X_train, X_test)
     y_pred = log_reg(X_train,y_train,X_test)
-    cm, score = get_results(y_test, y_pred)
+    cm, score = get_class_results(y_test, y_pred)
     print(cm)
     print("")
     print(f"The accuracy of the model is {score}")
+
+def run_lin_reg(X,y):
+    X_train, X_test, y_train, y_test = get_split(X,y)
+    X_train, X_test = standardize(X_train, X_test)
+    y_pred = lin_reg(X_train,y_train,X_test)
